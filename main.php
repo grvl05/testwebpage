fab6e210bc37fe55e03ab9c37cc78585598961b6
grvl05 <49820273+grvl05@users.noreply.github.com>
2020-04-19 08:35:27 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,15 +1,15 @@
 <?php
-$POST['login'];
-echo $login
+  $POST['login'];
+  echo $login
 
 
-$filename="pass.txt";
-$file=fopen($filename,"w");
-if($file==false)
-{
-echo "Eroor";
-exit();
-}
-fwrite($file,"111222333yesssssssss");
-fclose($file);
+  $filename="pass.txt";
+  $file=fopen($filename,"w");
+  if($file==false)
+  {
+  echo "Eroor";
+  exit();
+  }
+  fwrite($file,"111222333yesssssssss");
+  fclose($file);
 ?>
